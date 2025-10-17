47c16356ed4eb03a036f8b354aad702d561b5140
adam-miftah <adammiftah196@gmail.com>
2025-10-17 21:55:12 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import reportWebVitals from "./reportWebVitals";
 import { inject } from "@vercel/analytics";
 
-inject(); 
+inject();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
